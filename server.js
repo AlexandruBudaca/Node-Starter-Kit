63edd616062b905ed77dfc880f1d5bb3aab07939
Alex <alex.andrul10@gmail.com>
2020-06-23 21:32:15 +0100

    upd

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello Rocket!");
 });
+app.get("/node", (req, res) => {
+  res.send("Node + Express = Love");
+});
+app.get("/codeyourfuture", (req, res) => {
+  res.send({ cyf: "It's the best!" });
+});
+
+app.get("/chocolate", (req, res) => {
+  const { qty, flavour } = req.query;
+  res.send(`${qty}  Chocolates with   ${flavour}`);
+});
 
 app.listen(3000, () => {
   console.log("Listening to port 3000! 🚀");
